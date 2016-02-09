7cb60e92673abcbdb936c665c0fe95d1a15a5ee1
Petlefeu <nicolas_beguier@hotmail.com>
2016-02-09 17:16:38 +0100

    maj

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 # from multiprocessing import Process
 import numpy as np
 import sklearn as sk
-# import random
+import random
 from pdb import set_trace as st
 # import scipy as sp
 # from scipy import signal
@@ -26,6 +26,9 @@ CSV_DIRECTORY = "passengers"
 PICKLES_DIRECTORY = "pickles"
 TRAIN_FILE = 'train.csv'
 
+PERCENT_TRAINING = 70
+
+
 NB_CPU = 8
 
 
@@ -36,8 +39,13 @@ NB_CPU = 8
 def preload_into_pickles():
     """ Sauvegarde des informations en pickles """
     # Ne conserve que les entiers !
-    passengers_infos = np.genfromtxt("%s/%s" % (CSV_DIRECTORY, TRAIN_FILE), skip_header=True, delimiter=',')
-    np.save("%s/passengers.npy" % PICKLES_DIRECTORY, passengers_infos)
+    passengers_infos = np.genfromtxt(
+        "%s/%s" % (CSV_DIRECTORY, TRAIN_FILE),
+        skip_header=True,
+        delimiter=',',
+        # usecols=(0, 1, 2, 6, 7, 8, 9))
+        dtype=[('f0', 'i8'), ('f1', 'i8'), ('f2', 'i8'), ('f3', 'O'), ('f4', 'O'), ('f5', 'O'), ('f6', 'i8'), ('f7', 'i8'), ('f8', 'i8'), ('f9', 'O'), ('f10', 'f8'), ('f11', 'O'), ('f12', 'O')])
+    np.save("%s/passengers.npy" % PICKLES_DIRECTORY, np.array(passengers_infos.tolist()))
 
 def read_from_pickles():
     """ Lecture des pointeurs """
@@ -47,6 +55,13 @@ def read_from_pickles():
 ##        Features computation      ##
 ######################################
 
+def sex_to_int(passengers_infos_sex):
+    """ Cette fonction transforme le sexe en int :
+    male = 0
+    female = 1 """
+    passengers_infos_sex[passengers_infos_sex == 'male'] = 0
+    passengers_infos_sex[passengers_infos_sex == 'female'] = 1
+    return passengers_infos_sex
 
 def compute_features(passengers_infos):
     """ Cette fonction met en forme les attributs d'un individu pour qu'ils
@@ -55,6 +70,8 @@ def compute_features(passengers_infos):
     passengers_survived = passengers_infos[:, 1]
     passenger_id = passengers_infos[:, 0]
     passenger_pclass = passengers_infos[:, 2]
+
+    passenger_sex = sex_to_int(passengers_infos[:, 5])
     passenger_age = passengers_infos[:, 6]
     passenger_sibsp = passengers_infos[:, 7]
     passenger_parch = passengers_infos[:, 8]
@@ -62,9 +79,10 @@ def compute_features(passengers_infos):
     passenger_fare = passengers_infos[:, 10]
 
     features = [
-        passenger_fare
+        passenger_fare,
+        passenger_sex
     ]
-    return np.array(features).T, passengers_survived
+    return np.array(features).T.astype(float), passengers_survived.astype(int)
 
 
 ######################################
@@ -77,17 +95,35 @@ def train(passengers_infos):
     """ Cette fonction entraine un classifier
     grace aux features de passengers_infos """
 
-
     passengers_features, passengers_survived = compute_features(passengers_infos)
 
     # Training
+
     # cls = sk.linear_model.LogisticRegression(C=1)
-    # cls = sk.svm.SVC(C=0.01, probability=True)
-    # cls = sk.ensemble.RandomForestClassifier(n_estimators=200, max_features=None)
-    cls = sk.ensemble.AdaBoostClassifier()
+    # C1 67%, C100 67%, C0.01 50%
+
+    # cls = sk.svm.SVC(C=1, probability=True)
+    # C1 66%, C100 66%, C0.01 65%
+
+    cls = sk.ensemble.RandomForestClassifier(n_estimators=200, max_features=None)
+    # 200 75%, 20 74%, 1000 75%
+
+    # cls = sk.ensemble.AdaBoostClassifier()
+    # 50%
+
     cls.fit(passengers_features, passengers_survived)
 
     return cls
+
+def verify(classifier, passengers_infos):
+    """ Vérification des prédictions d'un classifier
+    sur un set de test """
+
+    passengers_features, passengers_survived = compute_features(passengers_infos)
+
+    passengers_survived_prediction = classifier.predict_proba(passengers_features)[:, 1]
+
+    return 1 - np.sum(abs(passengers_survived.astype(int) - passengers_survived_prediction.astype(float)))/len(passengers_survived)
 
 
 #########################################################
@@ -96,26 +132,38 @@ def train(passengers_infos):
 # preload_into_pickles()
 
 #########################################################
-##    Computing final predictions for all drivers      ##
+##    Computing final predictions for all passengers   ##
 #########################################################
 
 PASSENGERS_INFOS = read_from_pickles()
 
-CLASSIFIER = train(PASSENGERS_INFOS)
+PROBA = 0
 
+for i in range(5):
+    np.random.shuffle(PASSENGERS_INFOS)
+    NB_TRAINING_PASSENGERS = len(PASSENGERS_INFOS)*PERCENT_TRAINING/100
+    TRAINIG_SAMPLE = PASSENGERS_INFOS[:NB_TRAINING_PASSENGERS]
+    TESTING_SAMPLE = PASSENGERS_INFOS[NB_TRAINING_PASSENGERS:]
 
+    CLASSIFIER = train(TRAINIG_SAMPLE)
+    PROBA += verify(CLASSIFIER, TESTING_SAMPLE)
+    print PROBA/(i+1)
 
 #########################################################
 ##           Manual testing / feature creation         ##
 #########################################################
 
-T = []
+T_HOMMES = []
+T_FEMMES = []
 
 for t in np.linspace(0, 100, num=500):
-    T += [CLASSIFIER.predict_proba(t)[0][1]]
+    T_HOMMES += [CLASSIFIER.predict_proba([t, 0])[0][1]]
+    T_FEMMES += [CLASSIFIER.predict_proba([t, 1])[0][1]]
 
 plt.figure()
 plt.title('Survived')
-plt.plot(np.linspace(0, 100, num=500), np.array(T), '.-')
+plt.axis([0, 100, 0, 1])
+plt.plot(np.linspace(0, 100, num=500), np.array(T_HOMMES), '.-', color='blue')
+plt.plot(np.linspace(0, 100, num=500), np.array(T_FEMMES), '.-', color='red')
 
 plt.show()
